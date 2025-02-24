92ad1319b8ae727a6d40ae41374678522d2c6337
The-Lone-Cub <nsububenr@gmail.com>
2025-02-24 23:30:01 +0300

    Attempt no.4

--- send_whatsapp.py
+++ send_whatsapp.py
@@ -1,10 +1,10 @@
 import pywhatkit as kit
 
 # Replace with your parent's phone number (include country code, e.g., +256 for Uganda)
-phone_number = PHONE_NUMBER # Change to your parent's phone number
+phone_number = '+256700259998' # Change to your parent's phone number
 
 # Define the message you want to send
-message = MESSAGE
+message = "Hi, I've updated my Github account, please check it out [here](github.com/The-Lone-Cub/sdl-project). Thanks"
 
 # Send the message at a pecific time (24-hour format, adjust as needed)
 # Format: (phone_number, message)
