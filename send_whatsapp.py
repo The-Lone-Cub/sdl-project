52f3618cd9ff92d7adef2575fb76283ba2f3c9f4
The-Lone-Cub <nsububenr@gmail.com>
2025-02-24 22:36:25 +0300

    Trigger workflow for testing

--- send_whatsapp.py
+++ send_whatsapp.py
@@ -1,11 +1,11 @@
 import pywhatkit as kit
 
 # Replace with your parent's phone number (include country code, e.g., +256 for Uganda)
-phone_number = '+256700259998' # Change to your parent's phone number
+phone_number = PHONE_NUMBER # Change to your parent's phone number
 
 # Define the message you want to send
-message = 'Hey, I just updates my project on Github! Check it out: [your repo link].'
+message = MESSAGE
 
 # Send the message at a pecific time (24-hour format, adjust as needed)
 # Format: (phone_number, message, hour, minute)
-kit.sendwhatmsg(phone_number, message, 10+12, 5) # Sends at 3:00 PM, change time as needed
+kit.sendwhatmsg(phone_number, message, 10+12, 37) # Sends at 3:00 PM, change time as needed
